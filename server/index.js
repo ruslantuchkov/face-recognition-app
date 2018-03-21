d3565b009f7574afca6e68d3a38b4fd91d90a166
ruslantuchkov <nidjel87@gmail.com>
2018-03-21 17:46:36 +0500

    added communication with db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const knex = require('knex')({
+const bcrypt = require('bcrypt');
+const db = require('knex')({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
@@ -9,15 +10,79 @@ const knex = require('knex')({
   }
 });
 
-console.log(knex.select('*').from('users'));
+const salt = bcrypt.genSaltSync(10);
 
 const app = express();
 
 app.use(require('body-parser').json());
-app.use(require('cors'));
+app.use(require('cors')());
 
 app.get('/', (req, res) => {
   res.send('this is working');
+});
+
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+  db
+    .select('email', 'hash')
+    .where({ email })
+    .from('login')
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db('users')
+          .where({ email })
+          .then(user => res.json(user[0]))
+          .catch(err => res.status(400).json('unable to get user'));
+      } else {
+        res.status(400).json('wrong credentials');
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'));
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password, salt);
+  db.transaction(trx => {
+    trx
+      .insert({ hash, email })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .insert({ email: loginEmail[0], name, joined: new Date() })
+          .returning('*')
+          .then(user => res.json(user[0]))
+          .catch(err => res.status(400).json('unable to register'));
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  });
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db('users')
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('error getting user');
+      }
+    })
+    .catch(err => res.status(400).json('not found'));
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  db('users')
+    .where({ id })
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.json(entries[0]))
+    .catch(err => res.status(400).json('unable to get entries'));
 });
 
 app.listen('3000', () => console.log('server is running on port 3000'));
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -32,7 +32,7 @@ class Register extends React.Component {
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
-          <form className="measure">
+          <div className="measure">
             <fieldset id="register" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
@@ -80,13 +80,13 @@ class Register extends React.Component {
             </fieldset>
             <div className="">
               <input
-                onClick={() => this.props.onRouteChange('home')}
+                onClick={this.onSubmit}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Register"
               />
             </div>
-          </form>
+          </div>
         </main>
       </article>
     );
